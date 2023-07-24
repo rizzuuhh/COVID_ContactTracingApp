92c8add6a8bcaf4a1d095d5022114948a7703f03
rizza <denostarizza43@gmail.com>
2023-07-25 06:34:20 +1200

    Pseudocode

---
+++ ContactTracing.py
@@ -0,0 +1,10 @@
+# Get entry from user
+# Save data to the CSV file
+# Show a confirmation message
+# Create GUI window
+# Load the app photo
+# Create a label to display the app photo
+# Entry fields
+# Additional fields for positive cases
+# Additional field for contact cases
+# Save button
