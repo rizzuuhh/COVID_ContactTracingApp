85c48b6ec181d35be5e2b6d0a59a77dcc526759f
rizza <denostarizza43@gmail.com>
2023-07-25 06:44:01 +1200

    Ask user information

--- ContactTracing.py
+++ ContactTracing.py
@@ -1,5 +1,21 @@
+import tkinter as tk
+import csv
+import os
+
 # Get entry from user
-# Save data to the CSV file
+def save_entry():
+    status = test_status.get()
+    name = name_entry.get()
+    address = address_entry.get()
+    birthday = birthday_entry.get()
+    age = age_entry.get()
+    sex = sex_var.get()
+    phone_number = phone_entry.get()
+
+    if status == "Positive":
+        exposed_date = exposed_date_entry.get()
+        close_contacts = close_contacts_entry.get()
+    # Save data to the CSV file
 # Show a confirmation message
 # Create GUI window
 # Load the app photo
